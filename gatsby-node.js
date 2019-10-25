865dc30e6d3c5ed82be4777b74d0fc5b3830ffaa
Diyar Faraj <diyar.faraj@gmail.com>
2019-10-25 17:01:18 +0200

    chnages titles and pics

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 // Create pages from markdown files
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
+	const { createPage } = actions;
+	return new Promise((resolve, reject) => {
+		resolve(
+			graphql(
+				`
           query {
             services: allMarkdownRemark(
-              filter: { fileAbsolutePath: { regex: "/services/" } }
+              filter: { fileAbsolutePath: { regex: "/tjanster/" } }
               sort: { fields: [frontmatter___date], order: DESC }
             ) {
               edges {
@@ -57,40 +57,40 @@ exports.createPages = ({ graphql, actions }) => {
               }
             }
           }
-        `,
-      ).then((result) => {
-        result.data.services.edges.forEach(({ node }) => {
-          const component = path.resolve('src/templates/service.js');
-          createPage({
-            path: node.frontmatter.path,
-            component,
-            context: {
-              id: node.id,
-            },
-          });
-        });
-        result.data.team.edges.forEach(({ node }) => {
-          const component = path.resolve('src/templates/team.js');
-          createPage({
-            path: node.frontmatter.path,
-            component,
-            context: {
-              id: node.id,
-            },
-          });
-        });
-        result.data.testimonials.edges.forEach(({ node }) => {
-          const component = path.resolve('src/templates/testimonial.js');
-          createPage({
-            path: node.frontmatter.path,
-            component,
-            context: {
-              id: node.id,
-            },
-          });
-        });
-        resolve();
-      }),
-    );
-  });
+        `
+			).then((result) => {
+				result.data.services.edges.forEach(({ node }) => {
+					const component = path.resolve('src/templates/service.js');
+					createPage({
+						path: node.frontmatter.path,
+						component,
+						context: {
+							id: node.id
+						}
+					});
+				});
+				result.data.team.edges.forEach(({ node }) => {
+					const component = path.resolve('src/templates/team.js');
+					createPage({
+						path: node.frontmatter.path,
+						component,
+						context: {
+							id: node.id
+						}
+					});
+				});
+				result.data.testimonials.edges.forEach(({ node }) => {
+					const component = path.resolve('src/templates/testimonial.js');
+					createPage({
+						path: node.frontmatter.path,
+						component,
+						context: {
+							id: node.id
+						}
+					});
+				});
+				resolve();
+			})
+		);
+	});
 };
